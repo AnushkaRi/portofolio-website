727042ff51bbf058a398b5f7144ac92857f8fa15
Ana Dejanovic <ana.dejanovich@gmail.com>
2023-06-26 22:07:19 +0200

    Added Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "./styles.module.css";
 
 function Home() {
-  return <div className={styles.home}>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.home_container}>
+      <div className={styles.greeting}>Hi!</div>
+      <div className={styles.intro}>
+        I am <span className={styles.clr_red}>Ana</span>. Freshly gratuated{" "}
+        <span className={styles.clr_yellow}>Front-end Developer</span>.
+      </div>
+      <div className={styles.work}>
+        Check out my{" "}
+        <button className={styles.btn} onClick={() => navigate("/work")}>
+          work
+        </button>
+        .
+      </div>
+      <div className={styles.contact}>
+        And feel free to <span className={styles.clr_yellow}>contact</span> me.
+      </div>
+    </div>
+  );
 }
 
 export default Home;
