42ea49fa5052494ce23249bcb7611833214dd57e
Ana Dejanovic <ana.dejanovich@gmail.com>
2023-06-30 22:02:29 +0200

    Added asymmetric grid in Work page

--- src/pages/Illustration/Illustration.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./styles.module.css";
-
-function Illustration() {
-  return <div className={styles.illustration}>Illustration</div>;
-}
-
-export default Illustration;
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,7 +1,17 @@
 import styles from "./styles.module.css";
 
 function Work() {
-  return <div className={styles.work_container}></div>;
+  return (
+    <div className={styles.work_container}>
+      <div className={styles.radio}></div>
+      <div className={styles.pig_dice}></div>
+      <div className={styles.world_clock}></div>
+      <div className={styles.todo}></div>
+      <div className={styles.blackjack}></div>
+      <div className={styles.chat}></div>
+      <div className={styles.weather}></div>
+    </div>
+  );
 }
 
 export default Work;
