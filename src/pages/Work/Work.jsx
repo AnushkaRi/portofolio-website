bf97c7be59c2d3cb5c401ad27aa84e35a1ef3f7b
Ana Dejanovic <ana.dejanovich@gmail.com>
2023-07-01 22:21:29 +0200

    Added fade funcionality on work images

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -3,13 +3,27 @@ import styles from "./styles.module.css";
 function Work() {
   return (
     <div className={styles.work_container}>
-      <div className={styles.radio}></div>
-      <div className={styles.pig_dice}></div>
-      <div className={styles.world_clock}></div>
-      <div className={styles.todo}></div>
-      <div className={styles.blackjack}></div>
-      <div className={styles.chat}></div>
-      <div className={styles.weather}></div>
+      <div className={styles.radio}>
+        <p className={styles.img_description}>Radio Surfer Rosa</p>
+      </div>
+      <div className={styles.pig_dice}>
+        <p className={styles.img_description}>Pig Dice Game</p>
+      </div>
+      <div className={styles.world_clock}>
+        <p className={styles.img_description}>World Clock App</p>
+      </div>
+      <div className={styles.todo}>
+        <p className={styles.img_description}>To-Do List App</p>
+      </div>
+      <div className={styles.blackjack}>
+        <p className={styles.img_description}>Blackjack Game</p>
+      </div>
+      <div className={styles.chat}>
+        <p className={styles.img_description}>Chat App</p>
+      </div>
+      <div className={styles.weather}>
+        <p className={styles.img_description}>Weather App</p>
+      </div>
     </div>
   );
 }
