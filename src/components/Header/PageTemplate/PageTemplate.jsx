9661b17cbcb901cc1993139beb5b03dd5d18ebd1
Ana Dejanovic <ana.dejanovich@gmail.com>
2023-07-05 21:32:57 +0200

    Added Page Template

--- src/components/Header/PageTemplate/PageTemplate.jsx
+++ src/components/Header/PageTemplate/PageTemplate.jsx
@@ -1,7 +1,25 @@
+import { AiFillGithub } from "react-icons/ai";
+
 import styles from "./styles.module.css";
 
-function PageTemplate() {
-  return <div className={styles.wrapper}></div>;
+function PageTemplate(title, imgUrl, img_title, text, link) {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.image_wrapper}>
+        <img className={styles.img} src={imgUrl} alt="" />
+        <p className={styles.img_title}>{img_title}</p>
+        <img className={styles.img} src={imgUrl} alt="" />
+        <p className={styles.img_title}>{img_title}</p>
+      </div>
+      <div className={styles.text_wrapper}>
+        <div className={styles.text}>{text}</div>
+        <div className={styles.link}>
+          <AiFillGithub /> {link}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PageTemplate;
