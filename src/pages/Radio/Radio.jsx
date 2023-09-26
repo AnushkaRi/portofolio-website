8dda45572a0b6bd2eef920a5f7a0873d64897408
Ana Dejanovic <ana.dejanovich@gmail.com>
2023-09-26 21:35:57 +0200

    Added Radio image and text

--- src/pages/Radio/Radio.jsx
+++ src/pages/Radio/Radio.jsx
@@ -1,5 +1,5 @@
 import PageTemplate from "../../components/PageTemplate/PageTemplate";
-import ToDoPhoto from "../../assets/ToDo/ToDo.png";
+import RadioPhoto from "../../assets/Radio/radio.png";
 
 function Radio() {
   return (
@@ -7,8 +7,8 @@ function Radio() {
       {" "}
       <PageTemplate
         title="RADIO SURFER ROSA"
-        imgUrl_1={ToDoPhoto}
-        text_1="The most extensive coding project I've done was a Spotify clone app under the name Radio Surfer Rosa."
+        imgUrl_1={RadioPhoto}
+        text_1="The most extensive coding project I'm doing is a Spotify clone app under the name Radio Surfer Rosa."
         text_2="Consists of an inbuilt music player, music search engine, and Spotify Playlists."
         tech="React, Router, CSS and Spotify Web API"
         link="https://github.com/AnushkaRi/radio-surfer-rosa"
